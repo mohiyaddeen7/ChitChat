b83cafe7c67c186341ae30d2b656c37fdca2c3c7
mohiyaddeen7 <mohiyaddeenraza7@gmail.com>
2023-11-15 15:29:26 +0530

    create node server with socket.io

---
+++ js/client.js
@@ -0,0 +1,45 @@
+const socket = io("http://localhost:8000");
+
+const form = document.getElementById("sendBox");
+
+const messageInput = document.getElementById("messageInp");
+const messageSection = document.querySelector(".messageSection");
+
+let sendMessage = "";
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  sendMessage = messageInput.value;
+  appendFunc("You : " + sendMessage, "right");
+  socket.emit("send-message", sendMessage);
+  messageInput.value = "";
+});
+
+
+const userName = prompt("Enter your Name to join");
+
+const appendFunc = (message, position) => {
+  const messageElement = document.createElement("div");
+  messageElement.innerText = message;
+  if (position === "right") {
+    messageElement.classList.add("rightMesage");
+  } else {
+    messageElement.classList.add("leftMesage");
+  }
+  messageSection.append(messageElement);
+};
+
+socket.emit("new-user-joined", userName);
+
+socket.on("user-joined", (data) => {
+  appendFunc(data + " joined the chat", "right");
+});
+
+socket.on("receive", (data) => {
+  appendFunc(data.userName + " : " + data.message, "left");
+});
+
+
+socket.on("leftEvent",message=>{
+    appendFunc(message+" left the chat", "left")
+})
---
+++ nodeServer/index.js
@@ -0,0 +1,34 @@
+// Node Server which will handle socket io comnnections
+
+const io = require("socket.io")(8000, {
+  cors: {
+    origin: "*",
+  },
+});
+const users = {};
+
+io.on("connection", (socket) => {
+  //socket.on is used to handle for perticuar connection
+  socket.on("new-user-joined", (userName) => {
+    //storing all the users that are joined
+    users[socket.id] = userName;
+    console.log(userName + " joined");
+    //sends an event to the other users that are joined except the user that is joining
+    socket.broadcast.emit("user-joined", userName);
+  });
+
+  socket.on("send-message", (message) => {
+    socket.broadcast.emit("receive", {
+      message: message,
+      userName: users[socket.id],
+    });
+  });
+
+  socket.on('disconnect',message=>{
+    socket.broadcast.emit('leftEvent',users[socket.id]);
+    delete users[socket.id]
+  })
+  
+const count = io.engine.clientsCount
+console.log(count)
+});
